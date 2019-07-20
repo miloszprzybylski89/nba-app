37b6952a5aad11b2e41555f201c973d61a33b61e
Miłosz Przybylski <miloszprzybylski89@gmail.com>
2019-07-20 09:17:40 +0200

    Added redirect to landing page on logo click

--- nba-app/src/components/Logo.jsx
+++ nba-app/src/components/Logo.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 
 class Logo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            redirect: false,
         }
     }
+
+    handleLogoClick = () => {
+        this.setState({ redirect: true })
+    }
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to='./' />
+        }
+    }
+
     render() {
         return (
-            <span className='logo'><strong>NBA</strong> stats app</span>
+            <>
+                {this.renderRedirect()}
+                <span onClick={this.handleLogoClick} className='logo'><strong>NBA</strong> stats app</span>
+            </>
         );
     }
 }
