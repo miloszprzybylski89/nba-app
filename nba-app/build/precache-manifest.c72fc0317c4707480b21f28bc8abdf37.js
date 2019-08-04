88bb403b699d57fdd14cf7e1803073f3d37bcc33
Miłosz Przybylski <miloszprzybylski89@gmail.com>
2019-08-04 13:02:15 +0200

    build

---
+++ nba-app/build/precache-manifest.c72fc0317c4707480b21f28bc8abdf37.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51aa45fec738dd94f7f1f7ca1085374",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2965a61b2f59abc52d8",
+    "url": "./static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "99c4004fb75c070294d5",
+    "url": "./static/css/main.b0e94e26.chunk.css"
+  },
+  {
+    "revision": "c2965a61b2f59abc52d8",
+    "url": "./static/js/2.bb62122c.chunk.js"
+  },
+  {
+    "revision": "99c4004fb75c070294d5",
+    "url": "./static/js/main.1ad8d9f5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d3e21ae922bae8f83971d353a0bbf21a",
+    "url": "./static/media/LebronJames.d3e21ae9.jpg"
+  },
+  {
+    "revision": "7081da77ede993582d89fee375ffa0f4",
+    "url": "./static/media/LosAngelesLakers.7081da77.jpg"
+  },
+  {
+    "revision": "3c8d11aaa755ee887a63507e05ac363e",
+    "url": "./static/media/players.3c8d11aa.jpg"
+  },
+  {
+    "revision": "26b637c61c473a5307a4df69f5e4ff93",
+    "url": "./static/media/teamsList.26b637c6.jpg"
+  }
+]);
